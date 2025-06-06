99bbae481616099150c1d429250c819b8f4b58a0
Yorick Cleerbout <yorick.cleerbout@outlook.com>
2025-06-06 18:36:22 +0200

    Added Python & Dart Suggestions to the registryRouter

--- src/modules/registryRouter.ts
+++ src/modules/registryRouter.ts
@@ -1,9 +1,16 @@
 import { fetchNpmSuggestions } from './npm.js';
+import { fetchPypiSuggestions } from './pypi';
+import { fetchPubSuggestions } from './pub';
 
 export async function fetchSuggestionsByPrefix(prefix: string, query: string) {
     switch (prefix.toLowerCase()) {
         case 'npm':
             return fetchNpmSuggestions(query);
+        case 'pypi':
+            return fetchPypiSuggestions(query);
+        case 'dart':
+        case 'pub':
+            return fetchPubSuggestions(query);
         default:
             return [];
     }
