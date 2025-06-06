25ad22ae6001bcd3e6e9d26e1ba0d41ac06d9073
Yorick Cleerbout <yorick.cleerbout@outlook.com>
2025-06-06 18:56:49 +0200

    Created Utility function to escape Omnibox text

---
+++ src/modules/utils.ts
@@ -0,0 +1,7 @@
+export function escapeOmniboxText(text: string): string {
+    return text
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/'/g, '&quot;');
+}
